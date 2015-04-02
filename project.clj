c8bfc99cc9fb1d1790a6fd94657e7791883f207a
Tord Romstad <tord.romstad@gmail.com>
2015-04-02 11:00:46 +0200

    Upgrade to instaparse 1.3.6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-chess "0.1.0"
+(defproject clj-chess "0.1.1-SNAPSHOT"
   :description "A library of utilities for writing chess related applications."
   :url "http://github.com/romstad/clj-chess"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,6 @@
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [instaparse "1.3.5"]
+                 [instaparse "1.3.6"]
                  [me.raynes/conch "0.8.0"]
                  [org.apache.commons/commons-math3 "3.4.1"]])
