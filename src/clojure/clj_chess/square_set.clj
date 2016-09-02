ed030c371836fe6407bc88f229ad12673a3ca950
Tord Romstad <tord.romstad@gmail.com>
2016-09-02 09:03:17 +0200

    Various new square-set functions.

--- src/clojure/clj_chess/square_set.clj
+++ src/clojure/clj_chess/square_set.clj
@@ -1,6 +1,13 @@
 (ns clj-chess.square-set
-  (:refer-clojure :exclude [complement empty? first])
+  (:refer-clojure :exclude [complement contains? empty? first remove])
   (:import (chess SquareSet)))
+
+(defn square-set
+  "Creates a square-set with the given squares as elements."
+  [& squares]
+  (reduce (fn [result s] (bit-or (bit-shift-left 1 s) result))
+          0
+          squares))
 
 (defn empty?
   "True if the set has no elements."
@@ -66,3 +73,25 @@
   [set-1 set-2]
   (= set-1
      (union set-1 set-2)))
+
+(defn contains?
+  "Tests whether the square set contains the given square."
+  [set square]
+  (subset? (square-set square) set))
+
+(defn add
+  "Adds a new square to a square set."
+  [set square]
+  (union set (square-set square)))
+
+(defn remove
+  "Removes a square from a square set, if the square is already there.
+  If the square is not present in the square set, the set is returned
+  unchanged."
+  [set square]
+  (intersection set (complement (square-set square))))
+
+(defn pprint
+  "Pretty-print a square set to the standard output, for debugging."
+  [set]
+  (SquareSet/print set))
