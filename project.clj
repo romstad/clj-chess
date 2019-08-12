0664db014230469da4337333735cea29334dc627
Tord Romstad <tord.romstad@gmail.com>
2019-08-12 13:08:08 +0200

    Updated version to "0.5.1-SNAPSHOT".

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-chess "0.5.0"
+(defproject clj-chess "0.5.1-SNAPSHOT"
   :description "A library of utilities for writing chess related applications."
   :url "http://github.com/romstad/clj-chess"
   :license {:name "Eclipse Public License"
